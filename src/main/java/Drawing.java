7cbf48df06f71d76db1b447f6b4619bfdaf34e48
JayFung2580 <jyf20@ic.ac.uk>
2022-10-18 17:05:55 +0100

    drawing circle

---
+++ src/main/java/Drawing.java
@@ -0,0 +1,34 @@
+import com.sun.media.sound.FFT;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Drawing extends Canvas {
+    Point p=new Point(200,200);
+    Color c=new Color(0x992266);// The RGB number comprises three bytes: red, green and blue
+    Circle cir = new Circle(p,c,100);
+    private Frame f;
+
+    //constructor
+    public Drawing() {
+        f = new Frame("My window");
+        f.add(this);
+        f.setLayout(null);
+        f.setSize(400, 400);
+        f.setVisible(true);
+        f.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                f.dispose();
+            }
+        });
+        setBackground(Color.WHITE);
+        setSize(400, 400);
+
+    }
+
+    public void paint(Graphics g) {
+        cir.draw(g);
+    }
+
+}
